ba0b0c48fc9b53a37bd57e51ba41b07863b6ada1
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-07-13 11:01:05 +0100

    Configured router, passed in default route

--- src/app.js
+++ src/app.js
@@ -2,4 +2,13 @@ import bootstrap from 'bootstrap';
 
 export class App {
   message = 'Hello World!';
+
+  configureRouter(config, router) {
+    config.title = "Chris's Blog";
+
+    /* Pass an array of routes to config.map */
+    config.map([
+      { route: '', name: 'home', moduleId: 'posts/index', title: 'All Posts'}
+    ]);
+  }
 }
