2391b1aa0904cdc0c3f225a1bb36d5745f646aa4
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-07-13 14:01:12 +0100

    Injected PostService into the index view-model constructor

--- src/posts/index.js
+++ src/posts/index.js
@@ -1,5 +1,11 @@
+/* Allows usage of dependency injection, this ensures the same instance of PostService across the entire app */
+import {inject} from 'aurelia-framework';
+/* Import the PostService which contains the post data */
+import {PostService} from '../common/services/post-service';
+
+@inject(PostService)
 export class Index {
-  constructor() {
-    this.message = 'Hello world';
+  constructor(PostService) {
+    this.PostService = PostService;
   }
 }
