96b7ed7aa93aa59f54959e5c5658bc06b14c8c64
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-07-15 10:33:38 +0100

    Added comments

--- src/resources/value-converters/date-format.js
+++ src/resources/value-converters/date-format.js
@@ -1,6 +1,7 @@
 import moment from 'moment';
 
 export class DateFormatValueConverter {
+  /*  To refer to this value converter in a view, the convention is to use camel case (in this case it owuld be dateFormat) */
   toView(value) {
     /* In this case, the value being passed in is the date to be converted 
       The value() method comes from the moment library */
