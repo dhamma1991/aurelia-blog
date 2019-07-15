b56f6b737b366ceaefd1f04de0f40da9eb5ec4d9
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-07-15 10:24:45 +0100

    Using moment library's format method on the value in the date-format value converter

--- src/resources/value-converters/date-format.js
+++ src/resources/value-converters/date-format.js
@@ -2,7 +2,9 @@ import moment from 'moment';
 
 export class DateFormatValueConverter {
   toView(value) {
-    //
+    /* In this case, the value being passed in is the date to be converted 
+      The value() method comes from the moment library */
+    return moment(value).format('MMMM Do YYYY, h:mm a');
   }
 
   fromView(value) {
