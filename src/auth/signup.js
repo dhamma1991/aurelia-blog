7b7eb9fb7ffb2e0ade238745c7d66596cfb0caa2
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-07-22 00:53:43 +0100

    Added to signup() method and added error div to signup view

--- src/auth/signup.js
+++ src/auth/signup.js
@@ -7,5 +7,14 @@ export class Signup {
     this.authService = AuthService;
   }
 
-  signup()
+  signup() {
+    /* name is what was used for the two-way binding on the input on signup.html */
+    this.authService.signup(this.name).then(data => {
+      /* What happens here is very similiar to the login method found in login.js */
+      this.ea.publish('user', data.name);
+      this.router.navigateToRoute('login');
+    }).catch(error => {
+      this.error = error.message;
+    });
+  }
 }
