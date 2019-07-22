769d4a06d969fc420beb9d429f04e54043e094c3
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-07-22 16:13:02 +0100

    Injected AuthService into View view-model

--- src/posts/view.js
+++ src/posts/view.js
@@ -1,10 +1,12 @@
 import {inject} from 'aurelia-framework';
 import {PostService} from '../common/services/post-service';
+import {AuthService} from '../common/services/auth-service';
 
-@inject(PostService)
+@inject(PostService, AuthService)
 export class View {
-  constructor(PostService) {
+  constructor(PostService, AuthService) {
     this.postService = PostService;
+    this.authService = AuthService;
   }
 
   activate(params) {
