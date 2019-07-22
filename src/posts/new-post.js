d2fa179d4905716ff01e12bcb00c50b20987e3f5
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-07-22 12:26:16 +0100

    Using EventAggregator in new-post.js publishing post-updated

--- src/posts/new-post.js
+++ src/posts/new-post.js
@@ -1,10 +1,12 @@
 import {inject} from 'aurelia-framework';
 import {Router} from 'aurelia-router';
+import {EventAggregator} from 'aurelia-event-aggregator';
 import {PostService} from '../common/services/post-service';
 
-@inject(PostService, Router)
+@inject(PostService, Router, EventAggregator)
 export class NewPost {
-  constructor(PostService, Router) {
+  constructor(PostService, Router, EventAggregator) {
+    this.ea = EventAggregator;
     this.postService = PostService;
     this.router = Router;
   }
@@ -27,6 +29,8 @@ export class NewPost {
 
   newPost() {
     this.postService.create(this.post).then(data => {
+      /* Publish to something called post-updated, publish the date */
+      this.ea.publish('post-updated', Date());
       /* So when the post is added, the app goes to that post's view
         It does this by going to post-view and then passing through data.slug as the :slug required by that route */
       this.router.navigateToRoute('post-view', {slug: data.slug});
