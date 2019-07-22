9b30c50101673929c86cd99a65b09947b27296a5
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-07-22 01:55:22 +0100

    Set newPost() to navigate to the newly created post's view upon creation

--- src/posts/new-post.js
+++ src/posts/new-post.js
@@ -1,15 +1,19 @@
 import {inject} from 'aurelia-framework';
+import {Router} from 'aurelia-router';
 import {PostService} from '../common/services/post-service';
 
-@inject(PostService)
+@inject(PostService, Router)
 export class NewPost {
-  constructor(PostService) {
+  constructor(PostService, Router) {
     this.postService = PostService;
+    this.router = Router;
   }
 
   newPost() {
     this.postService.create(this.post).then(data => {
-      console.log(data);
+      /* So when the post is added, the app goes to that post's view
+        It does this by going to post-view and then passing through data.slug as the :slug required by that route */
+      this.router.navigateToRoute('post-view', {slug: data.slug});
     }).catch(error => {
       console.log(error);
     })
