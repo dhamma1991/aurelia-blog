c99885710570d15d14a367500ba654bf4f85a606
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-07-29 19:39:52 +0100

    Using toastr to handle errors in view and redirecting to home in case of error

--- src/posts/view.js
+++ src/posts/view.js
@@ -1,22 +1,29 @@
 import {inject} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import {Router} from 'aurelia-router';
 import {PostService} from '../common/services/post-service';
 import {AuthService} from '../common/services/auth-service';
 
-@inject(PostService, AuthService)
+@inject(PostService, AuthService, EventAggregator, Router)
 export class View {
-  constructor(PostService, AuthService) {
+  constructor(PostService, AuthService, EventAggregator, Router) {
     this.postService = PostService;
     this.authService = AuthService;
+    this.ea = EventAggregator;
+    this.router = Router;
   }
 
   activate(params) {
-    this.error = '';
     /* The find method here is provided by the post-service js
       It returns either the post or an error */
     this.postService.find(params.slug).then(data => {
       this.post = data.post;
     }).catch(error => {
-      this.error = error.message;
+      this.ea.publish('toast', {
+        type: 'error',
+        message: error.message
+      });
+      this.router.navigateToRoute('home');
     })
   }
 }
