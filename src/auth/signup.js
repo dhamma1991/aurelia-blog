56dccdb847d2d041ac894422fbc19c7f0ea69ae4
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-07-29 17:48:34 +0100

    Handling errors in signup using toast

--- src/auth/signup.js
+++ src/auth/signup.js
@@ -9,19 +9,19 @@ export class Signup {
     this.authService = AuthService;
     this.router = Router;
     this.ea = EventAggregator;
-    /* Assume the error doesn't exist until it actually does exist */
-    this.error = null;
   }
 
   signup() {
-    this.error = null;
     /* name is what was used for the two-way binding on the input on signup.html */
     this.authService.signup(this.name).then(data => {
       /* What happens here is very similiar to the login method found in login.js */
       this.ea.publish('user', data.name);
       this.router.navigateToRoute('home');
     }).catch(error => {
-      this.error = error.message;
+      this.ea.publish('toast', {
+        type: 'error',
+        message: error.message
+      });
     });
   }
 }
