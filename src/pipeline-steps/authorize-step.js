f86813133bbf0cd3b26651b0ac0dd25b8d58cf0d
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-07-29 15:29:00 +0100

    Checking for if the current user is null before sending to login

--- src/pipeline-steps/authorize-step.js
+++ src/pipeline-steps/authorize-step.js
@@ -1,15 +1,23 @@
 import {Redirect} from 'aurelia-router';
+import {inject} from 'aurelia-framework';
+import {AuthService} from '../common/services/auth-service';
 
+@inject(AuthService)
 export class AuthorizeStep {
+    constructor(AuthService) {
+        this.authService = AuthService;
+    }
     /* navigationInstruction is the information from the router configuration */
     /* next tells the app what to do next after authorization */
     run(navigationInstruction, next) {
         // If the user needs to be logged in, check for login
         /* The some() array function means that we just need one hit */
         if (navigationInstruction.getAllInstructions().some(i => i.config.settings.auth)) {
-            // Check for login
-            // If the user is not logged in, send them to login
-            return next.cancel(new Redirect('login'));
+            // Check for login, if the currentUser is null...
+            if (!this.authService.currentUser) {
+                // Send them to login
+                return next.cancel(new Redirect('login'));
+            }
         }
         return next();
     }
