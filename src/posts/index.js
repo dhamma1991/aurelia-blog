e234966d1aea03250ed7a6c04d3ca93d61921446
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-07-29 19:36:33 +0100

    Wired up index errors to use toast

--- src/posts/index.js
+++ src/posts/index.js
@@ -2,25 +2,25 @@
 import {inject} from 'aurelia-framework';
 /* Import the PostService which contains the post data */
 import {PostService} from '../common/services/post-service';
+import {EventAggregator} from 'aurelia-event-aggregator';
 
-@inject(PostService)
+@inject(PostService, EventAggregator)
 export class Index {
-  constructor(PostService) {
+  constructor(PostService, EventAggregator) {
     this.postService = PostService;
+    this.ea = EventAggregator;
   }
 
   /* This fires when the component is attached to the DOM */
   attached() {
-    /* Ensure error is clear when the component is first attached to the DOM
-      Catch will ensure that the error does get set if one is thrown */
-    this.error = '';
-
     /* Go get the posts data, and then... */
     this.postService.allPostPreviews().then(data => {
       this.posts = data.posts;
     }).catch(error => {
-      /* Set the error property of this view model to the contents of any errors thrown from the backend */
-      this.error = error.message;
+      this.ea.publish('toast', {
+        type: 'error',
+        message: error.message
+      });
     })
   }
 }
