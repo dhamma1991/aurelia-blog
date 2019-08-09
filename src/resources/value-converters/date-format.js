fb3db4b6ef3729acd00ce8feac10e8df72799fe3
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-08-09 14:55:25 +0100

    Added translating of post dates via changing locale of moment library

--- src/resources/value-converters/date-format.js
+++ src/resources/value-converters/date-format.js
@@ -13,9 +13,11 @@ export class DateFormatValueConverter {
   constructor(I18N) {
     this.i18n = I18N;
   }
-  
+
   /*  To refer to this value converter in a view, the convention is to use camel case (in this case it owuld be dateFormat) */
   toView(value) {
+    /* When the locale is changed, this will change the locale of the moment library */
+    moment.locale(this.i18n.getLocale());
     /* In this case, the value being passed in is the date to be converted 
       The value() method comes from the moment library */
     return moment(value).format('MMMM Do YYYY, h:mm a');
