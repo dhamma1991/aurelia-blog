934c1b2ce171d900eaa070668f5c219e1fcc5e87
Chris Hengmith <crhengmith@hotmail.co.uk>
2019-08-09 14:53:39 +0100

    Importing required modules into date-format.js for i18n value conversion, set i18n as property of DateFormatValueConverter constructor

--- src/resources/value-converters/date-format.js
+++ src/resources/value-converters/date-format.js
@@ -1,9 +1,19 @@
+import {inject} from 'aurelia-framework';
+import {I18N} from 'aurelia-i18n';
 import moment from 'moment';
+import 'moment/locale/en-gb';
+import 'moment/locale/fr';
 
+@inject(I18N)
 export class DateFormatValueConverter {
 
   /* Listen for the locale to change */
   signals = ['locale-changed'];
+
+  constructor(I18N) {
+    this.i18n = I18N;
+  }
+  
   /*  To refer to this value converter in a view, the convention is to use camel case (in this case it owuld be dateFormat) */
   toView(value) {
     /* In this case, the value being passed in is the date to be converted 
